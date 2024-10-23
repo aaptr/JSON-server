d98d276039e2f4467111002b1b780dafc2669a56
Oleksii Petrychenko <aa.petrichenko@gmail.com>
2024-10-23 17:21:29 +0200

    2nd

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(cors());
 app.use(express.json());
 
 
